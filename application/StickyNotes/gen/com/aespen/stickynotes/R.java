7a97b472aaf938fb94554d59e49569f24c2a0cd6
alan.edwardes <alan.edwardes@05d5b337-0000-4994-b06c-9c6b0019a67d>
2013-10-27 20:16:08 +0000

    Added a basic login page that alerts a username and password.
    git-svn-id: svn://alanedwardes.com/stickynotes/trunk@13 05d5b337-0000-4994-b06c-9c6b0019a67d

--- application/StickyNotes/gen/com/aespen/stickynotes/R.java
+++ application/StickyNotes/gen/com/aespen/stickynotes/R.java
@@ -10,6 +10,16 @@ package com.aespen.stickynotes;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black_opaque=0x7f060003;
+        public static final int blue_opaque=0x7f060001;
+        public static final int button_dark=0x7f060005;
+        public static final int light_blue_opaque=0x7f060002;
+        public static final int shadow=0x7f060004;
+        /**  awe: this is #AARRGGBB 
+         */
+        public static final int white_opaque=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,21 +31,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bluetolightbluegrad=0x7f020000;
+        public static final int buttonbackground=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int continueLogin=0x7f090004;
+        public static final int editTextEmail=0x7f090002;
+        public static final int editTextPassword=0x7f090003;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090000;
     }
     public static final class layout {
         public static final int activity_landing=0x7f030000;
     }
     public static final class menu {
-        public static final int landing=0x7f070000;
+        public static final int landing=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int email_text=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int login_button_text=0x7f050007;
+        public static final int login_text=0x7f050003;
+        public static final int password_text=0x7f050006;
+        public static final int welcome_text=0x7f050004;
     }
     public static final class style {
         /** 
@@ -59,10 +81,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonTextStyle=0x7f070003;
+        public static final int EditTextStyle=0x7f070002;
+        public static final int WelcomeFont=0x7f070004;
+        public static final int WelcomeFontSmall=0x7f070005;
     }
 }
---
+++ application/StickyNotes/src/com/aespen/stickynotes/Login.java
@@ -0,0 +1,42 @@
+package com.aespen.stickynotes;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        
+        Button btn = (Button) findViewById(R.id.continueLogin);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                continueLoginMethod(v);
+            }
+        });
+    }
+    
+    public void continueLoginMethod(View v) {
+		EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+		EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+		AlertDialog.Builder altDialog= new AlertDialog.Builder(this);
+		altDialog.setMessage(editTextEmail.getText().toString() + editTextPassword.getText().toString());
+		altDialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.landing, menu);
+        return true;
+    }
+}
