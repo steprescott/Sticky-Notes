254282cee3def6e88ce9406847bababe5197b1a1
Alan Edwardes <alan@alanedwardes.com>
2013-11-06 14:10:05 +0000

    The create note screen now persists data.

--- application/StickyNotes/src/com/aespen/stickynotes/NoteCreate.java
+++ application/StickyNotes/src/com/aespen/stickynotes/NoteCreate.java
@@ -1,7 +1,15 @@
 package com.aespen.stickynotes;
 
+import com.aespen.stickynotes.core.ServiceLocator;
+import com.aespen.stickynotes.persistence.ILocalRepository;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NoteCreate extends Activity {
     @Override
@@ -9,11 +17,34 @@ public class NoteCreate extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_notecreate);
         
+        
+        
         this.initialiseListeners();
     }
     
     protected void initialiseListeners()
     {
-
+    	Button btn = (Button) findViewById(R.id.createNoteButton);
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+            	createNotePressed(v);
+            }
+        });
+    }
+    
+    protected void createNotePressed(View v)
+    {
+    	ILocalRepository lr = ServiceLocator.getService(ILocalRepository.class);
+    	
+    	EditText txt = (EditText) findViewById(R.id.noteText);
+    	lr.createNote(txt.getText().toString());
+    	
+    	Toast.makeText(getApplicationContext(), "Note created", Toast.LENGTH_SHORT).show();
+    	
+    	Intent i = new Intent(this, NoteList.class);
+        startActivity(i);
     }
 }
