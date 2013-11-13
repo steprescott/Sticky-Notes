0756cbb1393f1d110dd42421a90ee778657df42d
Ste Prescott <mail@ste.me>
2013-11-13 14:03:14 +0000

    Added logic for communicating with the web service

--- application/StickyNotes/src/com/aespen/stickynotes/Login.java
+++ application/StickyNotes/src/com/aespen/stickynotes/Login.java
@@ -1,19 +1,35 @@
 package com.aespen.stickynotes;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.aespen.stickynotes.web.WebConnectionDelegate;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.accounts.NetworkErrorException;
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends Activity {
 
+	private WebConnectionDelegate connectionDelegate;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        connectionDelegate = new WebConnectionDelegate();
         
         Button btn = (Button) findViewById(R.id.continueLogin);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -25,13 +41,46 @@ public class Login extends Activity {
     }
     
     public void continueLoginMethod(View v) {
-		EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-		EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+		final EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+		final EditText editTextPassword = (EditText) findViewById(R.id.editTextPassword);
 
-		AlertDialog.Builder altDialog= new AlertDialog.Builder(this);
-		altDialog.setMessage(editTextEmail.getText().toString() + editTextPassword.getText().toString());
-		altDialog.show();
-    }
+		new AsyncTask<Void, Void, String>() {
+
+			@Override
+			protected String doInBackground(Void... params) {
+				try {
+					Map <String,String> parameters =  new HashMap<String,String>();
+					parameters.put("username", editTextEmail.getText().toString());
+					parameters.put("password", editTextPassword.getText().toString());
+					
+					JSONObject jsonObject = (JSONObject) connectionDelegate.postJsonAtPath("api/login", parameters);
+					
+					return jsonObject.toString();
+//					return jsonObject.optString("user");
+				} catch (NetworkErrorException e) {
+					Log.v("Error",e.toString());
+//					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+		
+			protected void onPostExecute(String result) {
+				if (result != null) {
+					Toast.makeText(Login.this, "POST : " + result, Toast.LENGTH_SHORT).show();
+				}
+			};
+			
+			
+		}.execute();
+	}
+//		AlertDialog.Builder altDialog= new AlertDialog.Builder(this);
+//		altDialog.setMessage(editTextEmail.getText().toString() + editTextPassword.getText().toString());
+//		altDialog.show();
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
