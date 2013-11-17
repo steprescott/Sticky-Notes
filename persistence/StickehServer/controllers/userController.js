750ae86a206376e9667adcfb68bb1dc300395827
Erik Nygren <erik.nygren6@gmail.com>
2013-11-17 11:59:21 +0000

    Updated the userController to interact with the ORM rather than using mock data.
    Sorted out a basic login request (does not open any session yet) and  the get user request now returns users from the database

--- persistence/StickehServer/controllers/userController.js
+++ persistence/StickehServer/controllers/userController.js
@@ -1,12 +1,57 @@
-var userModel = require('../models/userModel');
-exports.getUserData = function(req, res){
+var orm = require('../orm');
 
-    // does not care about the user id yet
-    var user = userModel.getUserData(1234);
-    res.send(200, user);
+exports.getUserData = function (req, res) {
+    var id = req.params.id;
+
+    var userModel = orm.model('user');
+    userModel.find({ where: {id: id} }).error(function (err) {
+
+        // error callback
+    }).success(function (user) {
+            if (!user)
+            {
+                res.send(401, 'Unknown ID');
+            }
+
+            res.send(200, user);
+        });
 };
 
-exports.loginRequest = function(req, res){
+exports.loginRequest = function (req, res) {
 
-    res.send(201, 'Well done you are logged in');
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var userModel = orm.model('user');
+    userModel.find({ where: {email: username} }).error(function (err) {
+
+        // error callback
+    }).success(function (user) {
+            if (!user)
+            {
+                res.send(403, 'Invalid credentials');
+                return;
+            }
+
+            // todo: hashing
+            if (user.dataValues.password !== password)
+            {
+                res.send(403, 'Invalid credentials');
+                return;
+            }
+
+            var session =
+            {
+                id: 12345,
+                created: new Date()
+            }
+
+            var result =
+            {
+                session: session,
+                user: user
+            }
+
+            res.send(200, result);
+        });
 };
