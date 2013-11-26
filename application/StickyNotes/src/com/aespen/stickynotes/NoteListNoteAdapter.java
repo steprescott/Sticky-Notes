20bc0492193abbcbc52331a764b15059acced926
Alan Edwardes <alan@alanedwardes.com>
2013-11-26 21:01:00 +0000

    Notes now display the date and time above the note contents on the note list.

--- application/StickyNotes/src/com/aespen/stickynotes/NoteListNoteAdapter.java
+++ application/StickyNotes/src/com/aespen/stickynotes/NoteListNoteAdapter.java
@@ -1,34 +1,33 @@
 package com.aespen.stickynotes;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.aespen.stickynotes.dao.Note;
 
 public class NoteListNoteAdapter extends android.widget.ArrayAdapter<Note>
 {
-	private LayoutInflater inflater;
-
 	public NoteListNoteAdapter(Context context, int resource)
 	{
 		super(context, resource);
-
-		inflater = LayoutInflater.from(context);
 	}
 
 	public View getView(int position, View convertView, ViewGroup parent)
 	{
+        NoteListRow noteListRow;
 		if (convertView == null)
 		{
-			convertView = inflater.inflate(R.layout.note_list_layout, parent, false);
+            noteListRow = new NoteListRow(this.getContext());
 		}
+        else
+        {
+            noteListRow = (NoteListRow) convertView;
+        }
 
-		TextView textView = (TextView) convertView.findViewById(R.id.note_list_item);
 		Note note = this.getItem(position);
-		textView.setText(note.getText());
-		return textView;
+        noteListRow.setNoteText(note.getText());
+        noteListRow.setNoteDate(note.getCreated());
+		return noteListRow;
 	}
 }
---
+++ application/StickyNotes/src/com/aespen/stickynotes/NoteListRow.java
@@ -0,0 +1,58 @@
+package com.aespen.stickynotes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import java.text.DateFormatSymbols;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Alan on 26/11/13.
+ */
+public class NoteListRow extends RelativeLayout
+{
+    private TextView noteText;
+    private TextView noteDate;
+    private TextView noteMonth;
+    private TextView noteTime;
+
+    public void setNoteText(String noteText)
+    {
+        this.noteText.setText(noteText);
+    }
+
+    public void setNoteDate(Date noteDate)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(noteDate);
+
+        int monthNumber = cal.get(Calendar.MONTH);
+        String monthName = this.getMonthNameFromMonthNumber(monthNumber);
+        this.noteMonth.setText(monthName.toUpperCase());
+
+        int date = cal.get(Calendar.DATE);
+        this.noteDate.setText(String.valueOf(date));
+
+        int hour = cal.get(Calendar.HOUR);
+        int minute = cal.get(Calendar.MINUTE);
+        this.noteTime.setText(String.format("%02d:%02d", hour, minute));
+    }
+
+    public NoteListRow(Context context)
+    {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.note_list_layout, this);
+        noteText = (TextView) findViewById(R.id.note_list_item_text);
+        noteDate = (TextView) findViewById(R.id.note_list_item_date);
+        noteMonth = (TextView) findViewById(R.id.note_list_item_month);
+        noteTime = (TextView) findViewById(R.id.note_list_item_time);
+    }
+
+    private String getMonthNameFromMonthNumber(int month)
+    {
+        return new DateFormatSymbols().getShortMonths()[month-1];
+    }
+}
