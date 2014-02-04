214ed2129d7b6e72bf0feb54437aa52d1eef1618
Ste Prescott <mail@ste.me>
2014-02-04 12:14:45 +0000

    Added useful constants file

---
+++ Sticky Notes/Sticky Notes/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  Sticky Notes
+//
+//  Created by Ste Prescott on 04/02/2014.
+//  Copyright (c) 2014 ste.me. All rights reserved.
+//
+
+#ifndef Sticky_Notes_Constants_h
+#define Sticky_Notes_Constants_h
+
+#define kBaseURL @"http://stickyapi.alanedwardes.com/"
+
+#define klogin @"user/login"
+
+#endif
